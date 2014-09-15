f7b36abb481e10f94d680ef074c26ef1498a251a
MSchwarzer-Haverbier <Michael.Schwarzer-Haverbier@ma-design.de>
2014-09-15 16:17:07 +0200

    Spotlight Klasse (Integrator) erstellt.

--- source/sl.Spotlight/Spotlight/Program.cs
+++ source/sl.Spotlight/Spotlight/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using sl.ConsolePortal;
 using sl.DateiSystemProvider;
 using sl.Domaene;
@@ -9,59 +7,20 @@ namespace Spotlight
 {
     class Program
     {
-        private static sl.kontrakte.IDateiSystemProvider _dateiSystemProvider = null;
-        private static sl.kontrakte.IConsolePortal _consolePortal = null;
-        private static sl.kontrakte.IDomäne _domäne = null;
-
-        private static string _pfad;
-        private static string _begriff;
-        private static string _dateiName;
-
         static void Main(string[] args)
         {
-            if (args.Count() != 2)
-            {
-                Console.WriteLine("Programm beendet! Bitte immer mit 2 Parametern starten");
-            }
-
-            _pfad = args[0];
-            _begriff = args[1];
-
             // Initialisiert die Bausteine
-            _dateiSystemProvider = new DateiSystemProvider();
-            _consolePortal = new ConsolePortal();
-            _domäne = new Domaene();
+            var dateiSystemProvider = new DateiSystemProvider();
+            var consolePortal = new ConsolePortal();
+            var domäne = new Domaene();
+
+            var spotlight = new Spotlight(dateiSystemProvider, domäne);
 
             // integrieren
-            Suchen(_pfad);
+            spotlight.Suchen(args[0], args[1],
+                consolePortal.TrefferAnzeigen,
+                consolePortal.FortschrittAnzeigen);
             Console.ReadLine();
-        }
-
-        private static void Suchen(string pfad)
-        {
-            _dateiSystemProvider.DateienBestimmen(pfad, Filtern, _consolePortal.FortschrittAnzeigen);
-        }
-
-        private static void Filtern(IEnumerable<string> dateinamen)
-        {
-            _domäne.Fortschreiten(dateinamen, dateiName =>
-            {
-                _dateiName = dateiName;
-                _dateiSystemProvider.DateiRelevant(dateiName, BeiDateiInhalt);
-            }, _consolePortal.FortschrittAnzeigen);
-
-            //_domäne.Fortschreiten(dateinamen, BeiDateiName, _consolePortal.FortschrittAnzeigen);
-        }
-
-        private static void BeiDateiName(string dateiName)
-        {
-            _dateiName = dateiName;
-            _dateiSystemProvider.DateiRelevant(dateiName, BeiDateiInhalt);
-        }
-
-        private static void BeiDateiInhalt(string dateiInhalt)
-        {
-            _domäne.DateiDursuchen(_dateiName, dateiInhalt, _begriff, _consolePortal.TrefferAnzeigen);
         }
     }
 }
---
+++ source/sl.Spotlight/Spotlight/Spotlight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using sl.kontrakte;
+
+namespace Spotlight
+{
+    public class Spotlight
+    {
+        private readonly IDateiSystemProvider _dateiSystemProvider;
+        private readonly IDomäne _domäne;
+
+        public Spotlight(IDateiSystemProvider dateiSystemProvider, IDomäne domäne)
+        {
+            _dateiSystemProvider = dateiSystemProvider;
+            _domäne = domäne;
+        }
+
+        public void Suchen(string pfad, string begriff, Action<string> beiTreffer, Action<int> beiFortschritt)
+        {
+            _dateiSystemProvider.DateienBestimmen(pfad,
+                dateinamen => Filtern(dateinamen, begriff, 
+                    beiTreffer, 
+                    beiFortschritt),
+                beiFortschritt);
+        }
+
+        private void Filtern(IEnumerable<string> dateinamen, string begriff, Action<string> beiTreffer, Action<int> beiFortschritt)
+        {
+            _domäne.Fortschreiten(dateinamen,
+                dateiName => _dateiSystemProvider.DateiRelevant(dateiName,
+                    dateiInhalt => _domäne.DateiDursuchen(dateiName, dateiInhalt, begriff,
+                        beiTreffer)),
+                beiFortschritt);
+        }
+    }
+}
