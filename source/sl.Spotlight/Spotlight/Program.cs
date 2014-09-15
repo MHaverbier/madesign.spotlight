ac0a27f495a199a8504608b8378e21c436df8d55
ClaudiaLange <claudia.lange@ma-design.de>
2014-09-15 14:24:58 +0200

    first step for integration, console still missing
    st

--- source/sl.Spotlight/Spotlight/Program.cs
+++ source/sl.Spotlight/Spotlight/Program.cs
@@ -3,31 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using sl.DateiSystemProvider;
+using sl.Domaene;
 
 namespace Spotlight
 {
     class Program
     {
+        private static sl.kontrakte.IDateiSystemProvider _dateiSystemProvider = null;
+        private static sl.kontrakte.IConsolePortal _consolePortal = null;
+        private static sl.kontrakte.IDomäne _domäne = null;
+
+        private static string _pfad;
+        private static string _begriff;
+        private static string _dateiName;
+
         static void Main(string[] args)
         {
             if (args.Count() != 2)
             {
                 Console.WriteLine("Programm beendet! Bitte immer mit 2 Parametern starten");
             }
-            var pfad = args[0];
-            var begriff = args[1];
 
+            // Initialisiert die Bausteine
+            _dateiSystemProvider = new DateiSystemProvider();
+            //_consolePortal = new ...
+            _domäne = new Domaene();
+
+            _pfad = args[0];
+            _begriff = args[1];
             
+            Suchen(_pfad, _begriff);
         }
 
         private static void Suchen(string pfad, string begriff)
         {
-            
+            _dateiSystemProvider.DateienBestimmen(pfad, Filtern, _consolePortal.FortschrittAnzeigen);
         }
 
-        private static void Filtern(IEnumerable<string> dateinamen, string begriff)
+        private static void Filtern(IEnumerable<string> dateinamen)
         {
-            
+            _domäne.Fortschreiten(dateinamen, foo,_consolePortal.FortschrittAnzeigen);
+        }
+
+        private static void foo(string dateiName)
+        {
+            _dateiName = dateiName;
+            _dateiSystemProvider.DateiRelevant(dateiName, BeiDateiInhalt);
+        }
+
+        private static void BeiDateiInhalt(string dateiInhalt)
+        {
+            _domäne.DateiDursuchen(_dateiName, dateiInhalt, _begriff, _consolePortal.TrefferAnzeigen);
         }
     }
 }
