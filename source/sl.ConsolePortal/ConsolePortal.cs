4bf4e893caf42a0236a644e42f5991fb59b540f9
ClaudiaLange <claudia.lange@ma-design.de>
2014-09-15 15:19:54 +0200

    Moved project to correct place

---
+++ source/sl.ConsolePortal/ConsolePortal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using sl.kontrakte;
+
+namespace sl.ConsolePortal
+{
+    public class ConsolePortal : IConsolePortal
+    {
+        public void TrefferAnzeigen(string dateiNamen)
+        {
+            Console.WriteLine(dateiNamen);
+        }
+
+        public void FortschrittAnzeigen(int fortschritt)
+        {
+            if (fortschritt == -1)
+            {
+                Console.WriteLine("Init");
+            }
+            else
+            {
+                Console.WriteLine(fortschritt + "%");
+            }
+        }
+    }
+}
