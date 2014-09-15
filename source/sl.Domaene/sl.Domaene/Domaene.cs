906b1f273f3514b3950b1b6a9edf97e72fa51281
Frank Lindecke <frank.lindecke@googlemail.com>
2014-09-15 12:41:48 +0200

    Domänen Komponente und Klasse angelegt

---
+++ source/sl.Domaene/sl.Domaene/Domaene.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sl.kontrakte;
+
+namespace sl.Domaene
+{
+    public class Domaene : IDomäne
+    {
+        public void Fortschreiten(IEnumerable<string> dateiNamen, Action<string> beiDateiName, Action<int> beiFortschritt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DateiDursuchen(string dateiNamen, string dateiInhalt, string begriff, Action<string> beiTreffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
