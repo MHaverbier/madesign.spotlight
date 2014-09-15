2e0fbd52e875e6370bcf6e5a29f3c77d5e00c8e7
ClaudiaLange <claudia.lange@ma-design.de>
2014-09-15 12:15:20 +0200

    fist step of reading args.

--- source/sl.Spotlight/Spotlight/Program.cs
+++ source/sl.Spotlight/Spotlight/Program.cs
@@ -10,6 +10,24 @@ namespace Spotlight
     {
         static void Main(string[] args)
         {
+            if (args.Count() != 2)
+            {
+                Console.WriteLine("Programm beendet! Bitte immer mit 2 Parametern starten");
+            }
+            var pfad = args[0];
+            var begriff = args[1];
+
+            
+        }
+
+        private static void Suchen(string pfad, string begriff)
+        {
+            
+        }
+
+        private static void Filtern(IEnumerable<string> dateinamen, string begriff)
+        {
+            
         }
     }
 }
