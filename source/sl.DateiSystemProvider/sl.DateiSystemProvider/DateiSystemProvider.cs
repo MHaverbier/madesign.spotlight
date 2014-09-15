4360ec6463d8e2f245726c9f8901766f302a59eb
MSchwarzer-Haverbier <Michael.Schwarzer-Haverbier@ma-design.de>
2014-09-15 12:44:23 +0200

    DSP Interface genutzt
    minor cleanup

--- source/sl.DateiSystemProvider/sl.DateiSystemProvider/DateiSystemProvider.cs
+++ source/sl.DateiSystemProvider/sl.DateiSystemProvider/DateiSystemProvider.cs
@@ -1,6 +1,17 @@
-﻿namespace sl.DateiSystemProvider
+﻿using System;
+using System.Collections.Generic;
+using sl.kontrakte;
+
+namespace sl.DateiSystemProvider
 {
-    public class DateiSystemProvider
+    public class DateiSystemProvider : IDateiSystemProvider
     {
+        public void DateienBestimmen(string pfad, Action<IEnumerable<string>> beiDateinamen, Action<int> beiFortschritt)
+        {
+        }
+
+        public void DateiRelevant(string dateiNamen, Action<string> beiDateiInhalt)
+        {
+        }
     }
 }
