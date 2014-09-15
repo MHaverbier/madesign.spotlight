f4a6d1ae2b0f267cb07ed146360420fb2a157860
Frank Lindecke <frank.lindecke@googlemail.com>
2014-09-15 14:13:30 +0200

    Unit Test Projekt für Domäne hinzugefügt

---
+++ source/sl.Domaene/sl.Domaene.Test/DomaeneTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using sl.Domaene;
+
+namespace sl.Domaene.Test
+{
+    [TestFixture]
+    public class DomaeneTest
+    {
+        [Test]
+        public void FortschreitenTest()
+        {
+            var domaene = new Domaene();
+
+            var dateien = new[] { "a", "b.txt", "c.dat", ".temp" };
+
+            var gefundeneDateien = new List<string>();
+            var fortschritt = new List<int>();
+            domaene.Fortschreiten( dateien, gefundeneDateien.Add, fortschritt.Add );
+
+            Assert.That( gefundeneDateien, Is.EqualTo( dateien ) );
+            Assert.That( fortschritt, Is.EqualTo( new int[] { 0, 25, 50, 100 } ) );
+        }
+    }
+}
